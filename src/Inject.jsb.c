f4dd31639dd4bf802497a9b81f4b5b7f88fa44a4
Minis User <user@minis.local>
2026-08-26 06:51:45 +0800

    Fix: update source files

--- src/Inject.jsb.c
+++ src/Inject.jsb.c
@@ -1,2 +1,2 @@
-[CONTEXT OFFLOADED] Content (~1253 tokens, 4142 bytes) saved to: /var/minis/offloads/tools/file_write_4b27a17894a0.txt
+[CONTEXT OFFLOADED] Content (~1299 tokens, 4240 bytes) saved to: /var/minis/offloads/tools/file_write_48349c3f2e12.txt
 Use file_read tool to retrieve if needed.
--- src/fishhook.c
+++ src/fishhook.c
@@ -1,2 +1,2 @@
-[CONTEXT OFFLOADED] Content (~1297 tokens, 4338 bytes) saved to: /var/minis/offloads/tools/file_write_4b534b0a7952.txt
+[CONTEXT OFFLOADED] Content (~2619 tokens, 10607 bytes) saved to: /var/minis/offloads/tools/file_write_4594a54d2a07.txt
 Use file_read tool to retrieve if needed.
